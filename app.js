08eac56fc4b98673ef3e69da4323934c9cd0af80
berry772 <56389423+berry772@users.noreply.github.com>
2020-04-23 18:27:30 +0900

    added ";"

--- app.js
+++ app.js
@@ -1,99 +1,99 @@
-ts = document.querySelector('#ts')
-ip = document.querySelector('#input')
-bs = document.querySelector('#btn-start')
-bp = document.querySelector('#btn-pause')
-bc = document.querySelector('#btn-cont')
+ts = document.querySelector('#ts');
+ip = document.querySelector('#input');
+bs = document.querySelector('#btn-start');
+bp = document.querySelector('#btn-pause');
+bc = document.querySelector('#btn-cont');
 
-timer = null
-clearInterval(timer)
+timer = null;
+clearInterval(timer);
 
-targetTime = null
-tempSec = null
+targetTime = null;
+tempSec = null;
 
-ts.innerHTML = '00:00'
-bs.style.display = ''
-bp.style.display = 'none'
-bc.style.display = 'none'
+ts.innerHTML = '00:00';
+bs.style.display = '';
+bp.style.display = 'none';
+bc.style.display = 'none';
 
 document.addEventListener(
   'click',
   e => {
     if (e.target === bs && ip.value !== '') {
-      timerStart(ip.value)
+      timerStart(ip.value);
     } else if (e.target === bp) {
-      timerPause()
+      timerPause();
     } else if (e.target === bc) {
-      timerStart(tempSec)
+      timerStart(tempSec);
     }
   },
   false
-)
+);
 ip.addEventListener('keyup', e => {
   if (e.keyCode === 13 && ip.value !== '') {
-    timerStart(ip.value)
+    timerStart(ip.value);
   }
-})
+});
 
 function timerStart(t) {
   if (timer) {
-    clearInterval(timer)
+    clearInterval(timer);
   }
-  setTargetTime(t)
-  tempSec = null
-  timer = setInterval(timerRepeat.bind(this), 47)
-  bs.style.display = 'none'
-  bp.style.display = ''
-  bc.style.display = 'none'
-}
+  setTargetTime(t);
+  tempSec = null;
+  timer = setInterval(timerRepeat.bind(this), 47);
+  bs.style.display = 'none';
+  bp.style.display = '';
+  bc.style.display = 'none';
+};
 
 function timerRepeat() {
-  const t = new Date().getTime()
+  const t = new Date().getTime();
   if (t >= targetTime) {
-    timerStop()
+    timerStop();
   } else {
-    ts.innerHTML = miliToClock(targetTime - t)
-    document.title = ts.innerHTML.slice(0, -4)
+    ts.innerHTML = miliToClock(targetTime - t);
+    document.title = ts.innerHTML.slice(0, -4);
   }
-}
+};
 
 function timerStop() {
-  clearInterval(timer)
-  ts.innerHTML = '00:00 Done'
-bs.style.display = ''
-bp.style.display = 'none'
-bc.style.display = 'none'
+  clearInterval(timer);
+  ts.innerHTML = '00:00 Done';
+bs.style.display = '';
+bp.style.display = 'none';
+bc.style.display = 'none';
   setTimeout(() => {
-    alert('done')
+    alert('done');
   }, 100)
-}
+};
 
 function timerPause() {
-  clearInterval(timer)
-  const tempSecond = targetTime - new Date().getTime()
-  tempSec = tempSecond / 1000
-  bs.style.display = ''
-  bp.style.display = 'none'
-  bc.style.display = ''
-}
+  clearInterval(timer);
+  const tempSecond = targetTime - new Date().getTime();
+  tempSec = tempSecond / 1000;
+  bs.style.display = '';
+  bp.style.display = 'none';
+  bc.style.display = '';
+};
 
 function setTargetTime(t) {
-  const ct = new Date().getTime()
+  const ct = new Date().getTime();
   if (t < 100) {
-    targetTime = ct + t * 1000
+    targetTime = ct + t * 1000;
   } else {
-    const min = Math.floor(t / 100)
-    targetTime = ct + min * 60 * 1000 + (t - min * 100) * 1000
+    const min = Math.floor(t / 100);
+    targetTime = ct + min * 60 * 1000 + (t - min * 100) * 1000;
   }
-}
+};
 
 function miliToClock(t) {
-  let second = [0, 0, 0]
-  second[0] = Math.floor(t / 1000 / 60)
-  second[1] = Math.floor((t - second[0] * 60 * 1000) / 1000)
-  second[2] = t - second[0] * 60 * 1000 - second[1] * 1000
+  let second = [0, 0, 0];
+  second[0] = Math.floor(t / 1000 / 60);
+  second[1] = Math.floor((t - second[0] * 60 * 1000) / 1000);
+  second[2] = t - second[0] * 60 * 1000 - second[1] * 1000;
   return `${second[0]
     .toString()
     .padStart(2, '0')}:${second[1]
     .toString()
-    .padStart(2, '0')}.${second[2].toString().padStart(3, '0')}`
-}
+    .padStart(2, '0')}.${second[2].toString().padStart(3, '0')}`;
+};
